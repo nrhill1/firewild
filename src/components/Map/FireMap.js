093aef1284a064c3b5aafd320105fe49ee1d3108
Nicolas Hill <nrhill1@gmail.com>
2020-11-12 12:11:00 -0800

    this branch sucks

--- src/components/Map/FireMap.js
+++ src/components/Map/FireMap.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Polygon } from 'google-maps-react';
+import { GoogleMap, LoadScript, Polygon } from '@react-google-maps/api';
 
 
+
+
+
+// These funcitons reverse coordinates to a format readable by Google Maps API
+/*
 const coord_pair_to_latlng =  ([lng, lat]) => ({ lat, lng })
 const convert_ring_coords = ring => ring.map(coord_pair_to_latlng)
+*/
 
-const mapStyles = {
-    margin: 30,
-    width: '93.75%',
-    height: '90%',
-    border: '1px solid #3E1C18',
-    display: 'inline-block'
+const mapContainerStyle = {
+  width: '93.75%',
+  height: '90%',
 };
 
+const fireOptions = {
+  fillColor     : "#BF5E4B",
+  fillOpacity   : 0.45,
+  strokeColor   : "#6B352A",
+  strokeOpacity : 0.9,
+  strokeWeight  : 1
+}
 
 class FireMap extends Component {
   
@@ -27,34 +37,39 @@ class FireMap extends Component {
       .then(data => this.setState({ fires: data.features }))
   }
 
-  displayFires = () => this.state.fires
-    .filter(fire => fire.geometry !== null)
-    .map(fire => fire.geometry.coordinates[0])
-    .map(rings => <Polygon 
-      paths = { rings.reduce((acc, ring) => acc.concat(convert_ring_coords(ring)), []) }
-      fillColor     = "#BF5E4B"
-      fillOpacity   = {0.45}
-      strokeColor   = "#6B352A"
-      strokeOpacity = {0.9}
-      strokeWeight  = {1}
-    />)
+  displayFires(){
+    return this.state.fires.map(fireJ => {
+      let fire = JSON.parse(fireJ.geoJSON)
+      let coordinates = fire.geometry.coordinates[0]
+      let coordArr = []
+      coordinates.map(coordinate => coordArr.push({lat:coordinate[1], lng:coordinate[0]} ))
+      return (
+        <Polygon
+          path={coordArr}
+          options={fireOptions}
+        />
+      )
+    })
+  }
 
 
   render (){
     return (
       <div className="mapBox">
-        <Map
-          google        = {this.props.google}
+      <LoadScript
+        googleMapsApiKey="AIzaSyBfd6BLe54h_8oTSb9mOz4yQnTLqGyReXs"
+      >
+        <GoogleMap
           zoom          = {8}
-          style         = {mapStyles}
-          initialCenter = {{ lat: 37.7749, lng: -122.4149 }}
+          mapContainerStyle    = {mapContainerStyle}
+          center        = {{ lat: 37.7749, lng: -122.4149 }}
         >
-          {this.displayFires()}
-        </Map>
+        </GoogleMap>
+      </LoadScript>
       </div>
     )
   }
 }
 
 
-export default GoogleApiWrapper({apiKey: 'AIzaSyBfd6BLe54h_8oTSb9mOz4yQnTLqGyReXs'})(FireMap)
+export default FireMap
