4c9c2b58e0168adc33512457352ce51cb3f5f684
Nicolas Hill <nrhill1@gmail.com>
2020-11-13 02:24:06 -0800

    some pretty cool style changes

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -11,18 +11,18 @@ class Navigbar extends Component {
           user: this.props.user
         }
     }
-    
+
 
     render() {
         return(
             <Navbar bg="black" className="navbar">
                 <ul>
-                    <li><Badge color="error" overlap="circle" badgeContent="0"><Nav.Link className="navLink" href="/">FireMap</Nav.Link></Badge></li>
+                    <li><Badge color="error" overlap="circle" badgeContent="0" variant="dot"><Nav.Link className="navLink" href="/">FireMap</Nav.Link></Badge></li>
                     { localStorage.token ? <li><Nav.Link className="navLink" href="/user">Profile</Nav.Link></li> : ""}
                     <li><Nav.Link className="navLink" href="/login">Login</Nav.Link></li>
                     <li><Nav.Link className="navLink" href="/register">Register</Nav.Link></li>
                     { localStorage.token ? <li><Nav.Link className="navLink" href="/report">Create Report</Nav.Link></li>: ""}
-                    <li><Badge color="error" badgeContent="0"><Nav.Link className="navLinkk" href="/feed">Report Feed</Nav.Link></Badge></li>
+                    <li><Badge color="error" badgeContent="0" overlap="circle" variant="dot"><Nav.Link className="navLinkk" href="/feed">Report Feed</Nav.Link></Badge></li>
                     { localStorage.token ? <li><Nav.Link className="navLink" onClick={this.props.handleLogout}>Logout</Nav.Link></li>: ""}
                 </ul>
             </Navbar>
