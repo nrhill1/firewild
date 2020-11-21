8c18aa07b07888a843e209fd21b8023635f3b6c4
Nicolas Hill <nrhill1@gmail.com>
2020-11-20 21:22:44 -0800

    getting rid of gmr logic in firemap

--- src/components/Map/FireMap.js
+++ src/components/Map/FireMap.js
@@ -1,40 +1,39 @@
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Polygon, InfoWindow } from 'google-maps-react';
-
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Polygon, InfoWindow } from "google-maps-react";
 
 const mapStyles = {
-    margin: 30,
-    width: '93.75%',
-    height: '90%',
-    border: '1px solid #3E1C18',
-    display: 'inline-block'
+  margin: 30,
+  width: "93.75%",
+  height: "90%",
+  border: "1px solid #3E1C18",
+  display: "inline-block"
 };
 
-
 class FireMap extends Component {
-  
   constructor(props) {
-    super(props)
-    this.state = { 
+    super(props);
+    this.state = {
       fires: [],
       activePoly: null,
       showingInfoWindow: false
-    }
+    };
   }
-  
+
   componentDidMount() {
-    fetch('https://opendata.arcgis.com/datasets/f72ebe741e3b4f0db376b4e765728339_0.geojson')
-      .then(res => res.json())
-      .then(data => this.setState({ fires: data.features }))
+    fetch(
+      "https://opendata.arcgis.com/datasets/f72ebe741e3b4f0db376b4e765728339_0.geojson"
+    )
+      .then((res) => res.json())
+      .then((data) => this.setState({ fires: data.features }));
   }
 
   handleClick = (props, polygon) => {
     const paths = polygon.getPaths().getArray();
-    const coordinates = paths.map(path => {
+    const coordinates = paths.map((path) => {
       const points = path.getArray();
-      return points.map(point => [point.lng(), point.lat()]);
+      return points.map((point) => [point.lng(), point.lat()]);
     });
-  
+
     polygon.setOptions({
       fillColor: "#6B352A",
       strokeColor: "#BF5E4B",
@@ -49,73 +48,26 @@ class FireMap extends Component {
         fillOpacity: 0.45,
         strokeColor: "#6B352A",
         strokeOpacity: 0.9,
-        strokeWeight: 1,
+        strokeWeight: 1
       });
     }
 
     this.setState({
       activePoly: polygon,
       showingInfoWindow: true
-    })
-  
+    });
+
     return {
-      type: 'Polygon',
+      type: "Polygon",
       coordinates
     };
-  }
-  
+  };
 
-  displayFires() {
-    return this.state.fires.map ((fire, idx) => {
-      let coordinates = fire.geometry.coordinates[0][0]
-      let coordArr = []
-      coordinates.map(coordinate => coordArr.push({lat:coordinate[1], lng:coordinate[0]} ))
-      return (
-        <Polygon 
-          key = {idx}
-          name = {
-            fire.properties.IncidentName 
-            + " Fire ----- Last Updated: " 
-            + fire.properties.DateCurrent.slice(5,10) + "/" 
-            + fire.properties.DateCurrent.slice(0,4)
-          }
-          paths = {coordArr}
-          fillColor     = "#BF5E4B"
-          fillOpacity   = {0.45}
-          strokeColor   = "#6B352A"
-          strokeOpacity = {0.9}
-          strokeWeight  = {1}
-          onClick = {this.handleClick}
-        />
-      )
-    })
-  }
+  displayFires() {}
 
-  render (){
-    return (
-      <div className="mapBox">
-        <Map
-          google        = {this.props.google}
-          zoom          = {8}
-          style         = {mapStyles}
-          initialCenter = {{ lat: 37.7749, lng: -122.4149 }}
-        >
-          {this.displayFires()}
-          <InfoWindow
-            position    = {this.state.activePoly}
-            anchor      = {this.state.activePoly}
-            visible     = {this.state.showingInfoWindow}
-            className   = "infoWindow"
-          >
-            <div>
-              <h1>{this.state.activePoly ? this.state.activePoly.name : ""}</h1>
-            </div>
-          </InfoWindow>
-        </Map>
-      </div>
-    )
+  render() {
+    return <div className="mapBox"></div>;
   }
 }
 
-
-export default GoogleApiWrapper({apiKey: 'AIzaSyBfd6BLe54h_8oTSb9mOz4yQnTLqGyReXs'})(FireMap)
+export default FireMap;
