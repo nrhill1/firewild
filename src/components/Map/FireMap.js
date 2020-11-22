48a86607acbb87a8c534503e005b61d9059689aa
Nicolas Hill <nrhill1@gmail.com>
2020-11-22 15:40:09 -0800

    trying to add layer from API

--- src/components/Map/FireMap.js
+++ src/components/Map/FireMap.js
@@ -9,20 +9,31 @@ class FireMap extends Component {
 
   componentDidMount() {
     // lazy load the required ArcGIS API for JavaScript modules and CSS
-    loadModules(["esri/Map", "esri/views/MapView"], { css: true }).then(
-      ([ArcGISMap, MapView]) => {
-        const map = new ArcGISMap({
-          basemap: "topo-vector"
-        });
+    loadModules(
+      [
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/FeatureLayer",
+        "esri/Graphic"
+      ],
+      { css: true }
+    ).then(([ArcGISMap, MapView, FeatureLayer, Graphic]) => {
+      const map = new ArcGISMap({
+        basemap: "topo-vector"
+      });
 
-        this.view = new MapView({
-          container: this.mapRef.current,
-          map: map,
-          center: [-118, 34],
-          zoom: 8
-        });
-      }
-    );
+      const fireLayer = new FeatureLayer({
+        url:
+          "https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/Public_Wildfire_Perimeters_View/FeatureServer/0/query?outFields=*&where=1%3D1"
+      });
+
+      this.view = new MapView({
+        container: this.mapRef.current,
+        map: map,
+        center: [-118, 34],
+        zoom: 8
+      });
+    });
   }
 
   componentWillUnmount() {
