2f7f013aaf884a82513a0300bac9a709ef82b6e8
Nicolas Hill <nrhill1@gmail.com>
2021-01-05 13:45:29 -0800

    merge plus string literal

--- src/components/Map/FireMap.js
+++ src/components/Map/FireMap.js
@@ -1,67 +1,62 @@
-import React, { Component } from "react";
-import { loadModules } from "esri-loader";
+import React, { Component } from 'react';
+import { loadModules } from 'esri-loader';
 
 class FireMap extends Component {
-  constructor(props) {
-    super(props);
-    this.mapRef = React.createRef();
-  }
+	constructor(props) {
+		super(props);
+		this.mapRef = React.createRef();
+	}
 
-  componentDidMount() {
-    // lazy load the required ArcGIS API for JavaScript modules and CSS
-    loadModules(
-      ["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer", "esri/PopupTemplate"],
-      { css: true }
-    ).then(([ArcGISMap, MapView, FeatureLayer]) => {
-      const map = new ArcGISMap({
-        basemap: "topo-vector"
-      });
+	componentDidMount() {
+		// lazy load the required ArcGIS API for JavaScript modules and CSS
+		loadModules(['esri/Map', 'esri/views/MapView', 'esri/layers/FeatureLayer', 'esri/PopupTemplate'], {
+			css: true
+		}).then(([ArcGISMap, MapView, FeatureLayer]) => {
+			const map = new ArcGISMap({
+				basemap: 'topo-vector'
+			});
 
-      this.view = new MapView({
-        container: this.mapRef.current,
-        map: map,
-        center: [-118, 34],
-        zoom: 8
-      });
+			this.view = new MapView({
+				container: this.mapRef.current,
+				map: map,
+				center: [-118, 34],
+				zoom: 8
+			});
 
-      var fireTemplate = {
-        title: function(event) {
-          const { graphic } = event
-          if (graphic.attributes.IncidentName){
-            return (`${graphic.attributes.IncidentName} Fire`)
-          }
-        },
-        content: function(event) {
-          const { graphic } = event
-          return (
-            `Acres: ${graphic.attributes.GISAcres}         
-            
-            Last Updated: ${graphic.attributes.DateCurrent}`
-          )
-        }
-      }
+			var fireTemplate = {
+				title: function(event) {
+					const { graphic } = event;
+					if (graphic.attributes.IncidentName) {
+						return `${graphic.attributes.IncidentName} Fire`;
+					}
+				},
+				content: function(event) {
+					const { graphic } = event;
+					return `Acres: ${graphic.attributes.GISAcres} \n        
+            Last Updated: ${graphic.attributes.DateCurrent}`;
+				}
+			};
 
+			const fireLayer = new FeatureLayer({
+				url:
+					'https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/Public_Wildfire_Perimeters_View/FeatureServer/0/query?outFields=*&where=1%3D1',
+				outFields: ['*'],
+				popupTemplate: fireTemplate
+			});
+			map.add(fireLayer);
+		});
+	}
 
-      const fireLayer = new FeatureLayer({
-        url:
-          "https://services3.arcgis.com/T4QMspbfLg3qTGWY/arcgis/rest/services/Public_Wildfire_Perimeters_View/FeatureServer/0/query?outFields=*&where=1%3D1",
-        outFields: ["*"],
-        popupTemplate: fireTemplate 
-      });
-      map.add(fireLayer);
-    });
-  }
+	componentWillUnmount() {
+		if (this.view) {
+			// destroy the map view
+			this.view.destroy();
+		}
+	}
 
-  componentWillUnmount() {
-    if (this.view) {
-      // destroy the map view
-      this.view.destroy();
-    }
-  }
-
-  render() {
-    return <div className="webmap" ref={this.mapRef} />;
-  }
+	render() {
+		return <div className="webmap" ref={this.mapRef} />;
+	}
 }
 
 export default FireMap;
